6710327610ae1ddb09fa8e383f872027815aa3c0
Isaac Aftergut <ijagut@vt.edu>
2023-09-21 14:57:11 -0400

    end of class

--- index.js
+++ index.js
@@ -15,16 +15,53 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
+app.get('/api/movies', async(req, res, next)=> {
+  try {
+    const SQL = `
+      SELECT *
+      FROM movies
+    `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+app.put('/api/movies/:id', async (req, res, next)=>{
+  try{
+      const SQL =`
+      UPDATE movies
+      SET title = $1, stars = $2
+      WHERE id = $3
+      RETURNING *
+
+      `;
+      const response = await client.query(SQL, [req.body.title, req.body.stars, req.params.id])
+      res.send(response.rows)
+  }catch(error){
+      next(error)
+  }
+})
 
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
-  `;
+  DROP TABLE IF EXISTS movies;
+  CREATE TABLE movies(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255),
+      stars INTEGER
+  );
+  INSERT INTO movies (title, stars) VALUES ('inception', 0);
+  INSERT INTO movies (title, stars) VALUES ('starwars', 0);
+  INSERT INTO movies (title, stars) VALUES ('startrek', 0)
+ 
+`
   console.log('create your tables and seed data');
-
-  const port = process.env.PORT || 3000;
+  await client.query(SQL)
+  const port = process.env.PORT || 4300;
   app.listen(port, ()=> {
     console.log(`listening on port ${port}`);
   });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const App = ()=> {
+  const [movies, setMovies] = useState([]);
+  useEffect(()=> {
+    const fetchMovies = async()=> {
+      const response = await axios.get('/api/movies');
+      setMovies(response.data);
+    };
+
+    fetchMovies();
+  },  []);
   return (
-    <h1>FullStack Template</h1>
+    <div>
+      <h1>Movies({ movies.length })</h1>
+      <ul>
+        {
+          movies.map( movie => {
+            return (
+              <li key={ movie.id }>
+                { movie.title } Stars:{movie.stars}
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
   );
 };
 
